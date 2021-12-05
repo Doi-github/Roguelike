c9c38a85d2c9aa5fe6ada248770de8d8f479ed85
kaname-san <kanameosu@gmail.com>
2021-12-06 04:02:24 +0900

    ２０＊２０のフィールドをランダムにエリア分けするようにした

--- RogueLike/Assets/Script/Map_Generation.cs
+++ RogueLike/Assets/Script/Map_Generation.cs
@@ -3,33 +3,45 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
+
 public class Map_Generation : MonoBehaviour
 {
 
     public int colums = 20, rows = 20;
+    
     
     public GameObject[] Floar;
     public GameObject[] Wall;
     public GameObject[] Enemy;
     public GameObject[] Exit;
     public GameObject[] Player;
+    public GameObject[] map_tile;
 
 
     public void SetupScene()
     {
-        BoardSetup();
+        /*BoardSetup();*/
+        Area map_area = new Area(null, null, -1, -1, colums, rows);
+        GameObject maptile;
+
+        maptile = map_tile[0];
+
+        map_area.splitRondom();
+        map_area.map_Gene(map_tile);
+
     }
 
     void Start()
     {
         SetupScene();
+        
         Instantiate(Player[0], new Vector3(2, 2, 0), Quaternion.identity);
     }
 
 
     void BoardSetup()
     {
-        int[] Map = Random_Map();
+        int[] Map = MapInitialize(colums,rows);
 
         for (int x = -1; x < colums + 1; x++)
         {
@@ -62,27 +74,27 @@ public class Map_Generation : MonoBehaviour
         }
     }
 
-    int[] Random_Map()
+    int[] MapInitialize(int colums,int rows)
     {
-        int[] Map = new int[400];
-        for (int i = 0; i < 400; i++)
+        int[] Map = new int[colums*rows];
+        for (int i = 0; i < colums*rows; i++)
         {
-            Map[i] = 0;
+            Map[i] = 1;
         }
 
         return Map;
     }
 
-    class Area
+    public class Area
     {
         Area parent;
-        Area[] child;
-        int x;
-        int y;
-        int width;
-        int height;
+        public Area[] child;
+        public float x;
+        public float y;
+        public float width;
+        public float height;
 
-        public Area(Area p,Area[] c, int x,int y, int w, int h)
+        public Area(Area p,Area[] c, float x, float y, float w, float h)
         {
             this.parent = p;
             this.child = c;
@@ -92,48 +104,116 @@ public class Map_Generation : MonoBehaviour
             this.height = h;
         }
 
-        public bool splitX(int d,Area area)
+        public bool splitX(float d)
         {
-            int minAreaWidth = 5;
-            if (d < minAreaWidth || area.width - d < minAreaWidth)
+            float minAreaWidth = 5;
+            float w = this.width - d;
+            if (d < minAreaWidth || w < minAreaWidth)
             {
                 return false;
             }
 
-            Area[] c = { new Area(area,new Area[2],area.x,area.y, d,area.height),
-                         new Area(area,new Area[2],area.x + d, area.y, area.width - d, area.height)};
+            Area[] c = { new Area(this, null, this.x, this.y, d, this.height),
+                         new Area(this, null, this.x + d, this.y, this.width - d, this.height)};
 
 
-            area.child = c;
+            this.child = c;
             
             return true;
         }
 
 
-        public bool splitY(int d,Area area)
+        public bool splitY(float d)
         {
-            int minAreaWidth = 5;
-            if (d < minAreaWidth || area.width - d < minAreaWidth)
+            float minAreaWidth = 5;
+            float h = this.height - d;
+            if (d < minAreaWidth || h < minAreaWidth)
             {
                 return false;
             }
 
-            Area[] c = { new Area(area,new Area[2],area.x,area.y, d,area.height),
-                         new Area(area,new Area[2],area.x + d, area.y, area.width - d, area.height)};
+            Area[] c = { new Area(this, null, this.x, this.y, this.width, d),
+                         new Area(this, null, this.x, this.y + d, this.width, h)};
 
 
-            area.child = c;
+            this.child = c;
 
             return true;
         }
 
         public void splitRondom()
         {
-            Area map_area;
+            Area t = this;
             int minwidth = 5;
+            bool ok; 
+            float d1 = (int)Math.Floor((double)UnityEngine.Random.Range(minwidth, t.width -minwidth));
+            float d2 = (int)Math.Floor((double)UnityEngine.Random.Range(minwidth, t.height - minwidth));
 
+            
+           
 
+            if (UnityEngine.Random.value > 0.5)
+            {
+               ok = t.splitX(d1);
+                Debug.Log("splitX");
+                Debug.Log(d1);
+            }
+            else
+            {
+               ok = t.splitY(d2);
+                Debug.Log("splitY");
+                Debug.Log(d2);
+            }
+
+            if (ok)
+            {
+                 foreach (Area a in t.child)
+                 {
+                    a.splitRondom();
+                 }
+            }
+           
+        }
+
+        public void map_Gene(GameObject[] map_tile)
+        {
+            GameObject maptile;
+            maptile = map_tile[0];
+            if(this.child != null)
+            {
+                foreach (Area a in this.child)
+                {
+                    a.map_Gene(map_tile);
+                }
+            }
+            else
+            {
+                maptile.transform.localScale = new Vector3(this.width, this.height, 0);
+                Instantiate(maptile, new Vector3(this.x + this.width / 2, this.y + this.height / 2, 0), Quaternion.identity);
+            }
+            
+        }
+        
+    }
+    
+    public class Room
+    {
+        float x;
+        float y;
+        float width;
+        float height;
+        Area area;
+
+        public Room(float x, float y, float width, float height, Area area)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.area = area;
         }
 
     }
+
+
 }
